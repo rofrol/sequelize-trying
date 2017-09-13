358b558561a7563483e3d9fa2b4a55985da6defa
Roman Frołow <rofrol@gmail.com>
2017-09-13 15:34:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://codeforgeek.com/2017/01/getting-started-sequelize-postgresql/
---
+++ setup_database.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+no_psql_in_path=true
+which psql && no_psql_in_path=false
+echo "no_psql_in_path: $no_psql_in_path"
+
+macos_setup() {
+    brew install postgres && \
+    brew services start postgresql
+}
+
+ubuntu_setup() {
+    echo "Installing on Linux"
+    echo deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main | sudo tee /etc/apt/sources.list.d/pgdg.list && \
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && \
+    sudo apt update && \
+    sudo apt install -y postgresql
+}
+
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        $no_psql_in_path && ubuntu_setup
+        cmd="sudo -u postgres psql"
+        ;;
+    Darwin*)
+        $no_psql_in_path && macos_setup
+        cmd="psql postgres" # Mac OS
+        ;;
+    MINGW*)
+	if [ $no_psql_in_path = true ]; then echo "Install postgresql and ensure psql is in %PATH%" && exit; fi
+        cmd="psql -U postgres" # Windows
+        ;;
+    *)
+esac
+echo ${cmd}
+
+DBNAME='sequelize'
+DBROLE='sequelize'
+DBPASS='mypass'
+
+eval "$cmd" << EOF
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='$DBNAME';
+DROP DATABASE IF EXISTS $DBNAME;
+CREATE DATABASE $DBNAME;
+DROP ROLE IF EXISTS $DBROLE;
+CREATE ROLE $DBROLE LOGIN PASSWORD '$DBPASS';
+ALTER DATABASE $DBNAME OWNER TO $DBROLE;
+EOF
